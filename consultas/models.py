b9b0f7dcda61bc0402acb482d497df6e3bb5201e
Arthur Medeiros <arthur.1995pc@gmail.com>
2025-10-22 11:12:25 -0300

    Fix

--- consultas/models.py
+++ consultas/models.py
@@ -4,7 +4,7 @@ class Gravacoes(models.Model):
     video = models.FileField(upload_to='gravacoes')
     data = models.DateTimeField()
     transcrever = models.BooleanField(default=False)
-    paciente = models.ForeignKey(motoristas, on_delete=models.DO_NOTHING)
+    motoristas = models.ForeignKey(motoristas, on_delete=models.DO_NOTHING)
     humor = models.IntegerField(default=0)
     transcricao = models.TextField()
     resumo = models.JSONField(default=list, blank=True)
--- qtv/settings.py
+++ qtv/settings.py
@@ -14,7 +14,9 @@ SECRET_KEY = 'django-insecure-etc9)19w^l^pb%vg7o)gr-%=0v-34jyx4dayv7ip@kizxkm-o-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    
+]
 
 
 #Aplicativos Ativo 
